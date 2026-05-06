261d9068cefbc1916a3d7233ceced32f784fe605
Thales Meier <meier.thales@gmail.com>
2026-05-05 23:08:04 -0300

    chore: get current version in install

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-GITHUB="https://github.com"
+GITHUB="https://api.github.com/repos"
 REPO="$GITHUB/thalestmm/dots"
 
 OS=$(uname -s)
 ARCH=$(uname -m)
 
 echo "$OS $ARCH"
+
+CURRENT_VERSION=$(curl -s "$REPO/releases/latest" | jq -r .tag_name)
+
+echo "Current version: $CURRENT_VERSION"
